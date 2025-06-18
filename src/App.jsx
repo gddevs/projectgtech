e66e1338b5a3df98b2770d80514b65b028326e4b
Gleidson Cavalcante Nascimento <gleidson.cavalcante@mapar.com>
2025-06-18 11:03:55 -0300

    atualizando componentes

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,19 @@
 import React from 'react';
 import Header from './components/Header.jsx';
 import Banner from './components/Banner.jsx';
+import { Componente1, Componente2, Componente3 } from './components/VariosComponentes.jsx';
+
 
 const App = () => {
   return (
     <div>
       <Header />
       <Banner />
+      <Componente1 />
+      <Componente2 />
+      <Componente3 />
     </div>
   );
 };
-
-
 
 export default App;
